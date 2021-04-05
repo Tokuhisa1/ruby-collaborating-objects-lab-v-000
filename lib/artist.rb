24822b1f148673f738a5b62e7130a745080eabba
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-04-05 15:20:43 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+end
